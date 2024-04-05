5a5ec261d45b0038a522464a94376ac87fa687cd
Anurag-git04 <shawanurag18@gmail.com>
2024-04-05 23:44:25 +0530

    ""

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -19,6 +19,15 @@ btns.forEach(function (btn) {
         else if(styles.contains('increase')){
             count++;
         }
+        if(count > 0){
+            value.style.color ="green"
+        }
+        else if(count < 0){
+            value.style.color ="red"
+        }
+        else{
+            value.style.color ="#222"
+        }
         value.textContent = count
     })
 });
